e067c5608aab3958fef82b8b220bddf8407289ae
sforsethi <rsraghavsethi@gmail.com>
2019-11-30 21:56:50 +0530

    Initial Commit

---
+++ Marsplay Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marsplay Assignment
+//
+//  Created by Raghav Sethi on 30/11/19.
+//  Copyright © 2019 Raghav Sethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
